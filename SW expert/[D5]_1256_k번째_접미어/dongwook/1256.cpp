ba41fe712dac441c312f3f397b5070e25e9fe836
Dong-wook94 <gygacpu@naver.com>
2019-07-03 02:29:03 +0900

    1256 k번째 접미어

---
+++ SW expert/[D5]_1256_k번째_접미어/dongwook/1256.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+void FindSubString(int t);
+int idx;
+char String[401];
+vector<string> arr;
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+	for (int t = 1; t <= testcase; t++) {
+		FindSubString(t);
+	}
+}
+
+void FindSubString(int t) {
+	string sub;
+	scanf("%d", &idx);
+	scanf("%s", String);
+	arr.push_back(sub.assign(String));
+
+	for (int i = 1; i < arr[0].length(); i++) {
+		sub = arr[0].substr(i, arr[0].length() - 1);
+		arr.push_back(sub);
+	}
+	sort(arr.begin(), arr.end());
+	strcpy(String, arr[idx - 1].c_str());
+	printf("#%d %s\n", t, String);
+	arr.clear();
+}
